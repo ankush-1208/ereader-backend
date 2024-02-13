d106920759d765955652a3b981b292c2d7e4aa76
Ankush Kumar <1208ankush@gmail.com>
2024-02-14 02:28:51 +0530

    Initial Commit

---
+++ src/test/java/com/ankush/readapp/ReadAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ankush.readapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
